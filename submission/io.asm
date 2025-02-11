4a0bec7085c01d372f8b301692c5e440003392dc
LuishCavada <164277347+LuishCavada@users.noreply.github.com>
2025-02-11 22:15:17 +0000

    Add files via upload

--- submission/io.asm
+++ submission/io.asm
@@ -5,10 +5,10 @@ x K =0
 y K =0
 soma K =0
 
-GD D000 ; le input x do teclado
+GD /000 ; le input x do teclado
 MM x   ; guarda variavel x
-GD D000
-GD D000 ; le input y do teclado
+GD /000
+GD /000 ; le input y do teclado
 MM y   ; guarda variavel y
 
 LD x 
@@ -28,6 +28,6 @@ MM soma
 JP finalizacao
 
 finalizacao LD soma
-PD E100
+PD /100
 
 
